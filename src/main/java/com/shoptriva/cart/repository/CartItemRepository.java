41d1eea94418c103ceafe81d364d9c8ecc95d192
Ritwik Saxena <ritwiksaxena19@gmail.com>
2026-09-11 22:22:28 +0530

    feat: add cart repositories

---
+++ src/main/java/com/shoptriva/cart/repository/CartItemRepository.java
@@ -0,0 +1,20 @@
+package com.shoptriva.cart.repository;
+
+import com.shoptriva.cart.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartItemRepository
+        extends JpaRepository<CartItem, Long> {
+
+    Optional<CartItem> findByCartIdAndProductId(
+            Long cartId,
+            Long productId
+    );
+
+    List<CartItem> findByCartId(Long cartId);
+
+    void deleteByCartId(Long cartId);
+}
